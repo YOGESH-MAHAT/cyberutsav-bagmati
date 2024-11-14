80d698294d7aed06d4c7896d86ea3679a1b4fe2f
YOGESH-MAHAT <lamoyogesh01@gmail.com>
2024-11-14 11:47:51 +0545

    Add files via upload

---
+++ body.js
@@ -0,0 +1,35 @@
+const questions = [
+    {
+        question:"How many balandor does cristiano ronaldo have?",
+        options:["10","6","0","5"],
+        answer:"5"
+    },
+
+    {
+        question:"What is the shape of football?",
+        options:["sperical","circular","oval","rectangular"],
+        answer:"circular"
+    },
+
+    {
+        question:"who s the goat of soccer?",
+        options:["messi","ronaldo","ozil","beckham"],
+        answer:"ronaldo"
+    },
+
+    {
+        question:"who won the previous balandor ?",
+        options:["Cristiano Ronaldo","Messut Ozil","zaltan ibramovic","vinicius junior"],
+        answer:"harry kane"
+    },
+    {
+        question:"How many balandor does cristiano ronaldo have?",
+        options:["10","6","0","5"],
+        answer:"5"
+    },
+    {
+        question:"How many balandor does cristiano ronaldo have?",
+        options:["10","6","0","5"],
+        answer:"5"
+    },
+];
